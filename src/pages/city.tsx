dd90600599b037ef62dc144e1faef47531ddd537
ozen <bluefirt15@gmail.com>
2023-02-09 12:54:25 -0500

    fix, url incorrecta

--- src/pages/city.tsx
+++ src/pages/city.tsx
@@ -16,7 +16,7 @@ export default function searchResult({ city, state }: Props) {
 
 export async function getServerSideProps({ query }: any) {
 	try {
-		const res = await fetch("http://localhost:3000/api/current-weather", {
+		const res = await fetch("/api/current-weather", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
